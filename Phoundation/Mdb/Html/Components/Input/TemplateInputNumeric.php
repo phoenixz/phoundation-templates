7fdc778cfef325dfb0fd6dc1796417c1dfbda094
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-03-11 11:44:59 -0700

    Fixed all libraries
    Fixed documentation
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Mdb/Html/Components/Input/TemplateInputNumeric.php
+++ Phoundation/Mdb/Html/Components/Input/TemplateInputNumeric.php
@@ -8,7 +8,7 @@
  * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
  * @copyright Copyright © 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @package Templates\Mdb
+ * @package Templates\Phoundation\Mdb
  */
 
 
